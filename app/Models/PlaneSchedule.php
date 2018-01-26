180c80baf37fbff4f245748e584f6672de54d5d3
Rafi <rafi160500@gmail.com>
2018-01-26 09:17:55 +0700

    Relasi Plane Schedue

--- app/Models/PlaneSchedule.php
+++ app/Models/PlaneSchedule.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Airport;
+use App\Models\Plane;
+use App\Models\Plane;
 
 class PlaneSchedule extends Model
 {
-    //
+    public function plane()
+    {
+      return $this->hasOne('Plane');
+    }
+    public function airport()
+    {
+      return $this->hasOne('Airport');
+    }
 }
