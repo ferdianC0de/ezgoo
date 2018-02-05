7ccbe2af8d63348c0442f30e3ef220e2a6d9e037
Ferdian Dwi Nugroho <ferdian.713.fn@gmail.com>
2018-02-05 07:47:13 +0700

    Fungsi booking

--- app/Http/Controllers/PlaneController.php
+++ app/Http/Controllers/PlaneController.php
@@ -140,33 +140,21 @@ class PlaneController extends Controller
         // TODO: Ambil variabel total buat banyaknya form
         $id = [$request->go,$request->back];
         $total = $request->total;
-        if ($request->type == "st") {
-          $planeSchedule = PlaneSchedule::find($request->go);
-          $bookings = new Booking();
-          $bookings->user_id = Auth::user()->id;
-          $bookings->booking_date = date('Y-m-d H:i:s');
-          $bookings->status = 1;
-          $bookings->type = "Pesawat";
-          $bookings->schedule_id = $request->go;
-          $bookings->save();
+        $userID = Auth::user()->id;
 
+        if ($request->type == "st") {
+          $planeSchedule = PlaneSchedule::find($id);
+          Booking::singleTrip(
+            $request->go,
+            $userID
+          );
         }elseif ($request->type == "rt" && count($id) == 2){
           $planeSchedule = PlaneSchedule::find($id);
-          $booking = new Booking();
-          $booking->user_id = Auth::user()->id;
-          $booking->booking_date = date('Y-m-d H:i:s');
-          $booking->status = 1;
-          $booking->type = "Pesawat";
-          $booking->schedule_id = $id[0];
-          $booking->save();
-
-          $booking2 = new Booking();
-          $booking2->user_id = Auth::user()->id;
-          $booking2->booking_date = date('Y-m-d H:i:s');
-          $booking2->status = 1;
-          $booking2->type = "Pesawat";
-          $booking2->schedule_id = $id[1];
-          $booking2->save();
+          Booking::singleTrip(
+            $request->go,
+            $request->back,
+            $userID
+          );
         }else{
           return back()->withAlert('Harap pilih penerbangan.');
         }
