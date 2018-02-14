81c611ff4dc583d25d1920d2d7f3b3f2043d120d
Rafi <rafi160500@gmail.com>
2018-02-14 11:53:31 +0700

    idk

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -9,208 +9,150 @@ use App\Models\DetailBooking;
 
 class BookingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
+      $this->plane = "App\Models\Plane";
+      $this->planeFare = "App\Models\PlaneFare";
+      $this->planeSchedule = "App\Models\planeSchedule";
+      $this->train = "App\Models\Train";
+      $this->trainFare = "App\Models\TrainFare";
+      $this->trainSchedule = "App\Models\trainSchedule";
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function search(Request $request)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function dBooking(Request $request)
-    {
-        $booking = Booking::all();
-        return view('frontend/booking', compact('booking'));
-    }
-
-    public function bookingPlane(Request $request)
-    {
-        //booking
-        $date = date('Y-m-d H:i:s');
-        $request->request->add(['booking_date' => $date]);
-        $data = request()->validate([
-            'booking_date'      => 'required',
-            'type'              => 'required',
-            'schedule_id'       => 'required'
-        ]);
-        $booking = Booking::create($data);
-        $request->request->add(['booking_id'=> $booking->id]);
-        //detailbooking
-        $dBooking = request()->validate([
-            'booking_id'        => 'required',
-            'passenger'        => 'required',
-            'class'             => 'required',
-
-        ]);
-        $dBooking = DetailBooking::create($dBooking);
-        $request->request->add(['detail_booking_id' => $dBooking->id]);
-        //passenger
-        $passenger = request()->validate([
-            'detail_booking_id' => 'required',
-            'name'              => 'required',
-
-        ]);
-        Passenger::create($passenger);
-        return redirect('frontend/book');
-
-      public function search(Request $request)
-      {
-        if ($request->baby <= $request->adult){
-          $total = [
-            'baby' => $request->baby,
-            'child'=> $request->child,
-            'adult'=> $request->adult
-          ];
-          $vehicle = $request->vehicle;
-          $type = $request->type;
-          $seat  = "";
-          $model = "";
-          //cek kendaraan
-          if ($vehicle == 'plane'){
-            $model = $this->planeSchedule;
-          }elseif($vehicle == 'train'){
-            $model = $this->trainSchedule;
-          }
-          //cek kursi
-          if ($request->class == "Ekonomi") {
-            $seat = 'eco_seat';
-          }elseif($request->class == "Bisnis") {
-            $seat = 'bus_seat';
-          }elseif($request->class == "First Class"){
-            $seat = 'first_seat';
-          }elseif($request->class == "Executive Class"){
-            $seat = 'exec_seat';
-          }
-          //cek tipe
-          if ($type == 'st'){
-            $schedule = $model::findSchedule($request->from, $request->destination, $request->date, $seat, count($total));
-            return view('booking.bookingSingle', compact('schedule', 'vehicle','type','total', 'seat'));
-            // return $schedule;
-          }elseif($type == 'rt'){
-            $scheduleG = $model::findSchedule($request->from, $request->destination, $request->date, $seat, count($total));
-            $scheduleB = $model::findSchedule($request->destination, $request->from, $request->dateB, $seat, count($total));
-            return view('booking.bookingRound', compact('scheduleG', 'scheduleB', 'vehicle','type','total', 'seat'));
-            // return $scheduleG;
-          }else{
-            abort(404);
-          }
-        }else{
-          return redirect('')->withError('Bayi tidak boleh lebih dari dewasa');
-        }
-      }
-      public function order(Request $request)
-      {
-        $model = "";
-        $class = "";
-        $vehicle = $request->vehicle;
-        $id = [$request->go,$request->back];
-        $fareTotal = 0;
-        $total = explode(',',$request->total);
-        $totalCount = $total[0] + $total[1] + $total[2];
+      if ($request->baby <= $request->adult){
         $total = [
-          'baby' => $total[0],
-          'child'=> $total[1],
-          'adult'=> $total[2]
+          'baby' => $request->baby,
+          'child'=> $request->child,
+          'adult'=> $request->adult
         ];
-        $seat = $request->seat;
-        if ($seat == 'eco_seat') {
-          $class = 'Ekonomi';
-        }elseif($seat == 'bus_seat'){
-          $class = 'Bisnis';
-        }elseif($seat == 'first_seat'){
-          $class = 'First Class';
-        }elseif($seat == 'exec_seat'){
-          $class = 'Eksekutif';
-        }
+        $vehicle = $request->vehicle;
+        $type = $request->type;
+        $seat  = "";
+        $model = "";
+        //cek kendaraan
         if ($vehicle == 'plane'){
           $model = $this->planeSchedule;
         }elseif($vehicle == 'train'){
           $model = $this->trainSchedule;
         }
-
-        if (isset($id) && isset($seat)) {
-          $schedule = $model::findWithPrice($id, $seat);
+        //cek kursi
+        if ($request->class == "Ekonomi") {
+          $seat = 'eco_seat';
+        }elseif($request->class == "Bisnis") {
+          $seat = 'bus_seat';
+        }elseif($request->class == "First Class"){
+          $seat = 'first_seat';
+        }elseif($request->class == "Executive Class"){
+          $seat = 'exec_seat';
+        }
+        //cek tipe
+        if ($type == 'st'){
+          $schedule = $model::findSchedule($request->from, $request->destination, $request->date, $seat, count($total));
+          return view('booking.bookingSingle', compact('schedule', 'vehicle','type','total', 'seat'));
+          // return $schedule;
+        }elseif($type == 'rt'){
+          $scheduleG = $model::findSchedule($request->from, $request->destination, $request->date, $seat, count($total));
+          $scheduleB = $model::findSchedule($request->destination, $request->from, $request->dateB, $seat, count($total));
+          return view('booking.bookingRound', compact('scheduleG', 'scheduleB', 'vehicle','type','total', 'seat'));
+          // return $scheduleG;
         }else{
           abort(404);
         }
-        return view('booking.bookingFix', compact('schedule','vehicle', 'total', 'totalCount', 'seat', 'class', 'fareTotal'));
+      }else{
+        return redirect('')->withError('Bayi tidak boleh lebih dari dewasa');
       }
-      public function fixOrder(Request $request)
-      {
-        return $request;
-        // if (Auth::check()) {
-          $modelV = "";
-          $modelF = "";
-          $modelS = "";
-          $vehicle = $request->vehicle;
-          $type = $request->type;
-          $id = $request->id;
-          // $userId = Auth::user()->id;
-          $total = $request->total;
-          $seat = $request->seat;
+    }
+    public function order(Request $request)
+    {
+      $model = "";
+      $class = "";
+      $vehicle = $request->vehicle;
+      $id = [$request->go,$request->back];
+      $fareTotal = 0;
+      $total = explode(',',$request->total);
+      $totalCount = $total[0] + $total[1] + $total[2];
+      $total = [
+        'baby' => $total[0],
+        'child'=> $total[1],
+        'adult'=> $total[2]
+      ];
+      $seat = $request->seat;
+      if ($seat == 'eco_seat') {
+        $class = 'Ekonomi';
+      }elseif($seat == 'bus_seat'){
+        $class = 'Bisnis';
+      }elseif($seat == 'first_seat'){
+        $class = 'First Class';
+      }elseif($seat == 'exec_seat'){
+        $class = 'Eksekutif';
+      }
+      if ($vehicle == 'plane'){
+        $model = $this->planeSchedule;
+      }elseif($vehicle == 'train'){
+        $model = $this->trainSchedule;
+      }
 
-          if ($vehicle == 'plane'){
-            $modelV = $this->plane;
-            $modelF = $this->planeFare;
-            $modelS = $this->planeSchedule;
-          }elseif($vehicle == 'train'){
-            $modelV = $this->train;
-            $modelF = $this->trainFare;
-            $modelS = $this->trainSchedule;
-          }
+      if (isset($id) && isset($seat)) {
+        $schedule = $model::findWithPrice($id, $seat);
+      }else{
+        abort(404);
+      }
+      return view('booking.bookingFix', compact('schedule','vehicle', 'total', 'totalCount', 'seat', 'class', 'fareTotal'));
+    }
+    public function fixOrder(Request $request)
+    {
+      return $request;
+      // if (Auth::check()) {
+        $modelV = "";
+        $modelF = "";
+        $modelS = "";
+        $vehicle = $request->vehicle;
+        $type = $request->type;
+        $id = $request->id;
+        // $userId = Auth::user()->id;
+        $total = $request->total;
+        $seat = $request->seat;
 
-          if (isset($request->type) && isset($id)) {
-            $math = $modelS::seatMath($total, $seat, $id);
-            return $math;
-          }else{
-            abort(404);
-          }
-        // }else{
-        //   return 'Register dulu baru bisa pesen';
-        // }
-      }
-      public function test()
-      {
-        return view('test.testTable');
-      }
-      public function testData(Datatables $datatables)
-      {
-        $query = Plane::select('*');
-        return $datatables->eloquent($query)->make(true);
-      }
-      //Note
-      //->make(true); selalu paling akhir
-      //Nambah kolom
-        //->addColumn('nama-kolom', function($bebas){
-        // apapun yang mau ditampilin, nanti isinya tinggal return
-        //})
-      //Edit kolom
-        //->editColumn('kolom-yang-diedit', function($bebas){
-        //sama aja kaya nambah, cuma ini ngedit yg udah ada
-        //})
-      //Kalo mau nambahin kolom + html
-        //->rawColumns(['action','lengthofcontract'])
+        if ($vehicle == 'plane'){
+          $modelV = $this->plane;
+          $modelF = $this->planeFare;
+          $modelS = $this->planeSchedule;
+        }elseif($vehicle == 'train'){
+          $modelV = $this->train;
+          $modelF = $this->trainFare;
+          $modelS = $this->trainSchedule;
+        }
+
+        if (isset($request->type) && isset($id)) {
+          $math = $modelS::seatMath($total, $seat, $id);
+          return $math;
+        }else{
+          abort(404);
+        }
+      // }else{
+      //   return 'Register dulu baru bisa pesen';
+      // }
+    }
+    public function test()
+    {
+      return view('test.testTable');
+    }
+    public function testData(Datatables $datatables)
+    {
+      $query = Plane::select('*');
+      return $datatables->eloquent($query)->make(true);
+    }
+    //Note
+    //->make(true); selalu paling akhir
+    //Nambah kolom
+      //->addColumn('nama-kolom', function($bebas){
+      // apapun yang mau ditampilin, nanti isinya tinggal return
+      //})
+    //Edit kolom
+      //->editColumn('kolom-yang-diedit', function($bebas){
+      //sama aja kaya nambah, cuma ini ngedit yg udah ada
+      //})
+    //Kalo mau nambahin kolom + html
+      //->rawColumns(['action','lengthofcontract'])
 }
--- routes/web.php
+++ routes/web.php
@@ -10,40 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-
-Route::get('/home', 'UserController@index')->name('home');
-Route::get('/edit/{id}/{type}', 'UserController@edit')->name('edit');
-Route::put('/update', 'UserController@update')->name('update');
-Route::resource('/customer','CustomerController');
-
-
-Route::group(["prefix" => 'admin'], function(){
-  Route::resource('/','AdminController');
-  Route::get('bookingdata','AdminController@bookingdata');
-  Route::get('train','AdminController@train');
-  Route::get('tprice','AdminController@tprice');
-  Route::get('plane','AdminController@plane');
-  Route::get('pprice','AdminController@pprice');
-  Route::post('create','AdminController@create');
-  Route::post('pcreate','AdminController@pcreate');
-
-  /*Edit*/
-  Route::get('edit/bookingdata/{id}', 'AdminController@ebookingData');
-  Route::post('edit/bookingdata/{id}', 'AdminController@ubookingData') ;
-  Route::get('edit/{id}','AdminController@ebookingData');
-  Route::put('update/{id}','AdminController@ubookingData');
-  Route::delete('delete/bookingdata/{id}','AdminController@dbookingData');
-
-
-});
-
 Route::group(['prefix'=> 'frontend'], function(){
   Route::get('/', function () {
       return view('frontend.home');
@@ -66,30 +32,41 @@ Route::group(['prefix'=> 'frontend'], function(){
   Route::get('/history', function () {
     return view('frontend.history');
   });
-  Route::post('/search', 'PlaneController@search');
-  Route::post('/order', 'PlaneController@order');
-  Route::post('/fixOrder', 'PlaneController@fixOrder');
-  Route::post('/booking', 'BookingController@bookingPlane');
+});
+
+Auth::routes();
+Route::get('/', 'HomeController@index');
+
+Route::group(['prefix' => 'booking'], function(){
+  Route::post('search', 'BookingController@search')->name('search');
+  Route::post('order', 'BookingController@order')->name('order');
+  Route::post('fixOrder', 'BookingController@fixOrder')->name('fixOrder');
 });
 
 Route::group(['prefix'=>'admin','middleware'=> 'checkRole'], function(){
-  Route::get('pesawat', 'AdminController@pesawat');
-  Route::get('kereta', 'AdminController@kereta_api');
-  Route::get('users', 'AdminController@showUsers');
-  Route::resource('', 'AdminController');
-  Route::resource('airport', 'AirportController');
+  Route::resource('/','AdminController');
+  Route::get('bookingdata','AdminController@bookingdata');
+  Route::get('train','AdminController@train');
+  Route::get('tprice','AdminController@tprice');
+  Route::get('plane','AdminController@plane');
+  Route::get('pprice','AdminController@pprice');
+  Route::post('create','AdminController@create');
+  Route::post('pcreate','AdminController@pcreate');
+
+  /*Edit*/
+  Route::get('edit/bookingdata/{id}', 'AdminController@ebookingData');
+  Route::post('edit/bookingdata/{id}', 'AdminController@ubookingData') ;
+  Route::get('edit/{id}','AdminController@ebookingData');
+  Route::put('update/{id}','AdminController@ubookingData');
+  Route::delete('delete/bookingdata/{id}','AdminController@dbookingData');
 });
 Route::group(['prefix' => 'user', 'middleware'=> 'checkRole'], function(){
   // Route::get('admin', 'AdminController@index')->name('admin');
-
   Route::get('home', 'HomeController@index')->name('home');
   Route::get('edit/{id}/{type}', 'UserController@edit')->name('edit');
   Route::put('update', 'UserController@update')->name('update');
   Route::put('updatePass', 'UserController@updatePassword')->name('updatePass');
 });
-<<<<<<< HEAD
-=======
-
 Route::group(['prefix' => 'test'], function(){
   Route::get('form', function(){
     return view('test.testForm');
@@ -97,4 +74,3 @@ Route::group(['prefix' => 'test'], function(){
   Route::get('test', 'BookingController@test');
   Route::get('testData', 'BookingController@testData');
 });
->>>>>>> d50d7fd8309ddfe074698494cd2d75ecdd31fc51
