13ac845ea33f3949a877fd5d5144d5df0a09e230
Muhammad Fahmi <mfahmifadh@gmail.com>
2018-02-21 08:03:36 +0700

    fahmi

--- resources/views/admin/train/listTrain.blade.php
+++ resources/views/admin/train/listTrain.blade.php
@@ -45,9 +45,9 @@
                 <td>{{ $data->eco_seat }}</td>
                 <td>{{ $data->bus_seat }}</td>
                 <td>{{ $data->exec_seat }}</td>
-                <td>{{ $data->trainfare->eco_seat }}</td>
-                <td>{{ $data->trainfare->bus_seat }}</td>
-                <td>{{ $data->trainfare->exec_seat }}</td>
+                <td>{{ number_format($data->trainfare->eco_seat) }}</td>
+                <td>{{ number_format($data->trainfare->bus_seat) }}</td>
+                <td>{{ number_format($data->trainfare->exec_seat) }}</td>
                 <td>
                   <form action="{{ url('admin/train/destroyTrain', $data->id) }}" method="post">
                       {{ csrf_field() }}
